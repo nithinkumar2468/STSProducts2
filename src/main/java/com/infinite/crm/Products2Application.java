cd60a9fa63eb6702a77b502ee8f52bd8c3d7a40c
nithinkumar2468 <nithinkumarkodipyaka@gmail.com>
2024-03-18 12:08:24 +0530

    commit

--- src/main/java/com/infinite/crm/Products2Application.java
+++ src/main/java/com/infinite/crm/Products2Application.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class Products2Application {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Products2Application.class, args);
 	}
