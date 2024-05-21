da248fd6af6e1eeb6244b14a933dbe98a75a75d4
nithinkumar2468 <nithinkumarkodipyaka@gmail.com>
2024-05-21 14:42:42 +0530

    deploytoazure

--- src/main/java/com/infinite/crm/service/Products2Service.java
+++ src/main/java/com/infinite/crm/service/Products2Service.java
@@ -10,37 +10,37 @@ import com.infinite.crm.repository.Products2Repository;
 
 @Service
 public class Products2Service {
-	
+
 	@Autowired
 	private Products2Repository prodRepo;
-	
+
 	public Products2 adduser(Products2 newProduct) {
-		// TODO Auto-generated method stub
+
 		return prodRepo.save(newProduct);
 	}
 
 	public List<Products2> productList() {
-		// TODO Auto-generated method stub
+
 		return prodRepo.findAll();
 	}
 
 	public Object findproduct(Long id) {
-		// TODO Auto-generated method stub
+
 		return prodRepo.findById(id);
 	}
 
 	public boolean exist(Long id) {
-		// TODO Auto-generated method stub
+
 		return prodRepo.existsById(id);
 	}
 
 	public void delete(Long id) {
-		// TODO Auto-generated method stub
+
 		prodRepo.deleteById(id);
 	}
 
-	public Products2 findById(Long id,Products2 newProduct) {
-		// TODO Auto-generated method stub
+	public Products2 findById(Long id, Products2 newProduct) {
+
 		return prodRepo.findById(id).map(product -> {
 			product.setPname(newProduct.getPname());
 			product.setRating(newProduct.getRating());
@@ -51,7 +51,7 @@ public class Products2Service {
 	}
 
 	public Products2 productById(Long id) {
-		// TODO Auto-generated method stub
+
 		return prodRepo.findById(id).get();
 	}
 
