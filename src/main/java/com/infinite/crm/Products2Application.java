44000f4241bfd9e32d00dcedd9cab3ec67a71d47
nithinkumar2468 <nithinkumarkodipyaka@gmail.com>
2024-05-28 15:36:50 +0530

    deploy-after-eureka

--- src/main/java/com/infinite/crm/Products2Application.java
+++ src/main/java/com/infinite/crm/Products2Application.java
@@ -2,10 +2,10 @@ package com.infinite.crm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class Products2Application {
 
 	public static void main(String[] args) {
